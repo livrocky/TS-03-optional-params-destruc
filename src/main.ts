93ac61ddf1007ed972f39bf1a5a73712d60482bf
rokas <rokasra@gmail.com>
2022-07-11 11:27:29 +0300

    html 5

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { items, Item } from './db/items.js';
 
 console.log('main.ts');
@@ -5,13 +6,12 @@ console.log('main.ts');
 console.log(items.length);
 
 // 3. sukurti funkcija kuri grazina visu kategoriju masyva.
-const getAllCategoriesArr = (arr: Item[]): string[] => {
-  const allCatsArr: string[] = arr.map((pObj: Item): string => pObj.category);
-  console.log('allCatsArr ===', allCatsArr);
-  return allCatsArr;
+const getSinglePropertyArr = (arr: Item[], property: 'category' | 'image'): string[] => {
+  const singlePropArr: string[] = arr.map((pObj: Item): string => pObj[property]);
+  return singlePropArr;
 };
 
-const allCatsArr: string[] = getAllCategoriesArr(items);
+const allCatsArr: string[] = getSinglePropertyArr(items, 'category');
 
 // 3.1 sukurti funkcija atrinkti is kategoriju tik unikalias reiksmes
 console.log([1, 2, 3].includes(1));
@@ -30,3 +30,7 @@ const getUniquesFromArr = (arr: string[]): string[] => {
 getUniquesFromArr(allCatsArr);
 
 // getUniquesFromArr - reduce
+
+// 5. sukurti funkcija kuri grazina masyva su visais paveiksleliais.
+const allImageArr = getSinglePropertyArr(items, 'image');
+console.log('allImageArr===', allImageArr);
