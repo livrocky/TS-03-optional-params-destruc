6f562f0f9da7403fab5b14d34f9cb7cb51cc2aaf
rokas <rokasra@gmail.com>
2022-07-11 11:17:21 +0300

    import fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
-/* eslint-disable no-console */
-import { items, Item } from './db/items';
+import { items, Item } from './db/items.js';
 
 console.log('main.ts');
 
 console.log(items.length);
 
-// 3. sukurti funkcija kuri grazina visu kategoriju masyva
-const getAllCategoriesArr = (arr:Item[]): string[] => {
+// 3. sukurti funkcija kuri grazina visu kategoriju masyva.
+const getAllCategoriesArr = (arr: Item[]): string[] => {
   const allCatsArr: string[] = arr.map((pObj: Item): string => pObj.category);
-  console.log('allCatsArr===', allCatsArr);
+  console.log('allCatsArr ===', allCatsArr);
   return allCatsArr;
 };
-getAllCategoriesArr(items);
+
+const allCatsArr: string[] = getAllCategoriesArr(items);
+
+// 3.1 sukurti funkcija atrinkti is kategoriju tik unikalias reiksmes
+console.log([1, 2, 3].includes(1));
+
+const getUniquesFromArr = (arr: string[]): string[] => {
+  const uniqueArr: string[] = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const str of arr) {
+    if (!uniqueArr.includes(str)) {
+      uniqueArr.push(str);
+    }
+  }
+  console.log('uniqueArr ===', uniqueArr);
+  return uniqueArr;
+};
+getUniquesFromArr(allCatsArr);
+
+// getUniquesFromArr - reduce
